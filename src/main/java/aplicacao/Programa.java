50a5833a68652053ce3d9b747a12a1ed0d9a2c4b
JuniorAlves05 <junioralveswork01@gmail.com>
2023-04-15 00:08:01 -0300

    Primeiro contato com banco de dados

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,34 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+			Pessoa p1 = new Pessoa(null,"Carlos Da Silva","carlos@gmail.com");
+			Pessoa p2 = new Pessoa(null,"Joaquim Torres","joaquim@gmail.com");
+			Pessoa p3 = new Pessoa(null,"Ana Maria","ana@gmail.com");
+			
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+			EntityManager em = emf.createEntityManager();
+			
+			Pessoa p = em.find(Pessoa.class, 2);
+			em.getTransaction().begin();
+			em.remove(p);
+			em.getTransaction().commit();
+			
+			System.out.println("Pronto ! ");
+			
+			em.close();
+			emf.close();
+			
+			
+			
+			}
+}
+;
